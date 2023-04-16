f867015eea1fcd8d93b91c438f2b67427177c84d
Dorad <ddxi@qq.com>
2023-04-16 03:59:08 +0200

    fix bug.

--- src/index.js
+++ src/index.js
@@ -12,15 +12,16 @@ function isJson(str) {
   return false;
 }
 
+var picBedConfig = {};
+var migrate_image = core.getInput("migrate_image") === "true";
+
 // test the picBed config
 if (!isJson(core.getInput("picBedConfig"))) {
   core.warning("picBedConfig is not a valid json string, use default config: {}, and set migrate_image to false.");
-  core.setInput("migrate_image", "false");
+  migrate_image = false;
 }
 
-var picBedConfig = {};
-
-if (core.getInput("migrate_image") === "true") {
+if (migrate_image) {
   core.info("migrate_image is true, use picBedConfig to upload images.");
   picBedConfig = JSON.parse(core.getInput("picBedConfig"));
 }
@@ -28,7 +29,7 @@ if (core.getInput("migrate_image") === "true") {
 let config = {
   notion_secret: core.getInput("notion_secret"),
   database_id: core.getInput("database_id"),
-  migrate_image: core.getInput("migrate_image") === "true",
+  migrate_image: migrate_image,
   picBed: picBedConfig,
   status: {
     name: core.getInput("status_name"),
